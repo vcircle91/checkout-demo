f4be913b4e059e03470bded2e9b31cb77411dc29
tkloy <tobias.kloy@computop.com>
2022-01-04 14:17:36 +0100

    Added button

--- js/main.js
+++ js/main.js
@@ -4,15 +4,19 @@ const cardCheckbox = document.querySelector('#cardCheckbox');
 const cardBody = document.querySelector('#cardBody');
 const idealCheckbox = document.querySelector('#idealCheckbox');
 const idealBody = document.querySelector('#idealBody');
+const payButton = document.querySelector('#pay');
 
 // Initial setup
 idealBody.style.display = "none";
 cardBody.style.display = "none";
 invoiceBody.style.display = "none";
+payButton.style.display = "none";
 
 invoiceCheckbox.addEventListener('change', function () {
     if (this.checked) {
         invoiceBody.style.display = "block";
+        payButton.textContent = "Pay by Invoice";
+        payButton.style.display = "block";
         // Hide card
         cardCheckbox.checked = false;
         cardBody.style.display = "none";
@@ -28,6 +32,8 @@ invoiceCheckbox.addEventListener('change', function () {
 cardCheckbox.addEventListener('change', function () {
     if (this.checked) {
         cardBody.style.display = "block";
+        // Hide button
+        payButton.style.display = "none";
         // Hide invoice
         invoiceCheckbox.checked = false;
         invoiceBody.style.display = "none";
@@ -42,6 +48,8 @@ cardCheckbox.addEventListener('change', function () {
 idealCheckbox.addEventListener('change', function () {
     if (this.checked) {
         idealBody.style.display = "block";
+        payButton.textContent = "Pay with iDEAL";
+        payButton.style.display = "block";
         // Hide invoice
         invoiceCheckbox.checked = false;
         invoiceBody.style.display = "none";
